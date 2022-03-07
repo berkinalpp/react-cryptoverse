10cb1c1e11b9e7288463d588fecdf22b98b22fb5
Berkin Alp <berkinalpp@gmail.com>
2022-03-07 17:18:01 +0300

    Page layout was completed

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import {Layout, Typography, Space} from "antd";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  CryptoDetails,
+  Cryptocurrencies,
+  News,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />}></Route>
+              <Route path="/exchanges" element={<Exchanges />}></Route>
+              <Route
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              ></Route>
+              <Route path="/crypto/:coinId" element={<CryptoDetails />}></Route>
+              <Route path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{color: "white", textAlign: "center"}}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
